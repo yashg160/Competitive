00266b86759ca60d92ac07efa859a9bcc1b2c885
yashg160 <yashg160@gmail.com>
2020-05-08 14:37:16 +0530

    Added challenge

---
+++ MayLeetCoding/CheckStraightLine.java
@@ -0,0 +1,27 @@
+public class CheckStraightLine {
+
+    public static double findSlope(int[] set1, int[] set2) {
+        return ((double) set1[1] - (double) set2[1]) / ((double) set1[0] - (double) set2[0]);
+    }
+
+    public static boolean checkStraightLine(int[][] coordinates) {
+        if (coordinates.length == 2)
+            return true;
+
+        int n = coordinates.length;
+        double slope = findSlope(coordinates[0], coordinates[1]);
+
+        for (int i = 1; i <= n - 2; i++) {
+            if (slope != findSlope(coordinates[i], coordinates[i + 1]))
+                return false;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[][] coordinates = new int[][] { { -4, -3 }, { 1, 0 }, { 3, -1 }, { 0, -1 } };
+
+        System.out.println(checkStraightLine(coordinates));
+    }
+}
